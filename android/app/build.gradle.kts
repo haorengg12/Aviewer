4985489f4587508982e1387e199c9ce855e9bfcd
Harley Huang <haorengg12@foxmail.com>
2026-02-21 22:06:30 +0800

    6546

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,13 +32,13 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.app"
+        applicationId = "com.haoren.aviewer"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        versionName = System.getenv("VERSION_NAME") ?: flutter.versionName
     }
 
     signingConfigs {
---
+++ android/app/src/main/kotlin/com/haoren/aviewer/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.haoren.aviewer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
